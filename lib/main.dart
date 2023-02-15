b6e9bdee0aa85e60a4d1df0694335bff659f3d12
soumyaa03 <82571869+soumyaa03@users.noreply.github.com>
2023-02-16 01:39:59 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:streamcom/controller/auth_methods.dart';
+import 'package:streamcom/models/utils/colors.dart';
+import 'package:streamcom/providers/user_provider.dart';
+import 'package:streamcom/view/screens/landing_screen.dart';
+import 'package:streamcom/view/screens/login_screen.dart';
+import 'package:streamcom/view/screens/signup_screen.dart';
+import 'package:streamcom/models/user.dart' as model;
+import 'package:streamcom/view/widgets/loading.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(
+      create: (_) => UserProvider(),
+    ),
+  ], child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Streamcom',
+      theme: ThemeData.light().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+        appBarTheme: AppBarTheme.of(context).copyWith(
+          backgroundColor: backgroundColor,
+          elevation: 0,
+          titleTextStyle: const TextStyle(
+            color: primaryColor,
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+          iconTheme: const IconThemeData(
+            color: primaryColor,
+          ),
+        ),
+      ),
+      routes: {
+        LandingScreen.routeName: (context) => const LandingScreen(),
+        LoginScreen.routeName: (context) => const LoginScreen(),
+        SignupScreen.routeName: (context) => const SignupScreen(),
+        HomeScreen.routeName: (context) => const HomeScreen(),
+      },
+      home: FutureBuilder(
+        future: AuthMethods()
+            .getCurrentUser(FirebaseAuth.instance.currentUser != null
+                ? FirebaseAuth.instance.currentUser!.uid
+                : null)
+            .then((value) {
+          if (value != null) {
+            Provider.of<UserProvider>(context, listen: false).setUser(
+              model.User.fromMap(value),
+            );
+          }
+          return value;
+        }),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Loading();
+          }
+
+          if (snapshot.hasData) {
+            return const HomeScreen();
+          }
+          return const LandingScreen();
+        },
+      ),
+    );
+  }
+}
